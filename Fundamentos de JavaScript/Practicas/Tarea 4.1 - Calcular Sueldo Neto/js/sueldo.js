086ae35d39914ec942616e60c355b347f1c901e3
Victor Morales <victormorales_1588@hotmail.com>
2020-08-07 00:48:37 -0400

    calculo sobre el sueldo bruto

--- Fundamentos de JavaScript/Practicas/Tarea 4.1 - Calcular Sueldo Neto/js/sueldo.js
+++ Fundamentos de JavaScript/Practicas/Tarea 4.1 - Calcular Sueldo Neto/js/sueldo.js
@@ -1,31 +1,58 @@
 const form = document.getElementById("form");
 const message = document.getElementById("messageHelp");
 const render = document.getElementById("result");
+
+// funcion para procesar el sueldo
 const procesar = (e) => {
     e.preventDefault();
 
+    // variables para hacer los calculos 
     let afp = 0.0287;
     let ars = 0.0304;
-    let isr = 0.15;
+    let isr1 = 0.15;
+    let isr2 = 0.2;
+    let isr3 = 0.25;
     let t_descuento = 0;
     let s_neto = 0;
+    let r_isr = 0;
+    let exento = 399923;
+    let monto = 0;
+    let impuesto = 0;
 
-    const s_bruto = document.getElementById("sueldo_bruto");
+    const s_bruto = document.getElementById("sueldo_bruto"); // capturo el campo del sueldo
 
+    // hago algunas validaciones para procesar el sueldo
     if (s_bruto.value !== "") {
         r_afp = s_bruto.value * afp;
         r_ars = s_bruto.value * ars;
-        r_isr =  ? : "No cumple";
-        if (s_bruto.value > 34260) {
-            let valor1 = s_bruto.value * 12; 
+        t_descuento = r_afp + r_ars;
+        s_neto = s_bruto.value - Math.trunc(t_descuento);
+        r_isr = (s_neto * 12) > exento ? s_neto * 12 : "Exento";
+
+        // valido si el monto es excento y procede a hacer el calculo
+        if (r_isr > exento) {
+            // forma no. 1 con el 15% del ISR
+            if (r_isr > 399923) {
+                monto = r_isr - exento;
+                impuesto = (monto * isr1) / 12;
+            }
+
+            // forma no. 2 con el 20% del ISR
+            if (r_isr > 599884) {
+                monto = r_isr - exento;
+                impuesto = ((monto * isr2) + 29994) / 12;
+            }
+            
+            // forma no. 3 con el 25% del ISR
+            if (r_isr > 3833171) {
+                monto = r_isr - exento;
+                impuesto = ((monto * isr3) + 76652) / 12;
+            }
 
         } else {
-            r_isr = "Exede";
+            impuesto = r_isr; // si no supera el exento, imprime el mensaje
         }
-        t_descuento = r_afp + r_ars;
-        s_neto = s_bruto.value - t_descuento;
-        
-        console.log(r_afp,r_ars, r_isr);
+
 
         render.innerHTML = `
         <div class="form-group">
@@ -40,7 +67,7 @@ const procesar = (e) => {
             
             <div class="form-group">
                 <label for="isr">ISR</label> 
-                <input type="text" class="form-control" id="isr" value="${r_isr}" disabled readonly>
+                <input type="text" class="form-control" id="isr" value="${impuesto}" disabled readonly>
             </div> 
             
             <div class="form-group">
@@ -55,8 +82,21 @@ const procesar = (e) => {
         `;
 
     } else {
-        message.innerHTML = "Debe llenear el campo";        
+        message.innerHTML = "Debe llenear el campo";
     }
 }
 
-form.addEventListener("submit", procesar);
+// funcion para cancelar y eliminar los datos del campo sueldo bruto
+const cancelar = (e) => {
+    render.innerHTML = "";
+    document.getElementById("sueldo_bruto").focus();
+}
+
+form.addEventListener("submit", procesar); // evento para el submit
+form.addEventListener("reset", cancelar); // evento para el cancelar de un formulario
+
+/*  
+     ***********   Nota:  ******************
+
+    mi logica no es muy buena pero espero que pueda ser entendida
+*/
