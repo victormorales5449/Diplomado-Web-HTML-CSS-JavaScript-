9bc0f01196a38ef07979cca356460390b7402b7d
Victor Morales <victormorales_1588@hotmail.com>
2020-08-14 01:11:00 -0400

    modificacion de la calculadora del sueldo

--- Fundamentos de JavaScript/Practicas/Tarea 4.1 - Calcular Sueldo Neto/js/sueldo.js
+++ Fundamentos de JavaScript/Practicas/Tarea 4.1 - Calcular Sueldo Neto/js/sueldo.js
@@ -5,7 +5,8 @@ const render = document.getElementById("result");
 // funcion para procesar el sueldo
 const procesar = (e) => {
     e.preventDefault();
-
+    // Math.trunc() esta funcion es para extraer numeros enteros
+    
     // variables para hacer los calculos 
     let afp = 0.0287;
     let ars = 0.0304;
@@ -15,7 +16,9 @@ const procesar = (e) => {
     let t_descuento = 0;
     let s_neto = 0;
     let r_isr = 0;
-    let exento = 399923;
+    let exento = 416220.01;
+    let exento2 = 624329.01;
+    let exento3 = 867123.01;
     let monto = 0;
     let impuesto = 0;
 
@@ -23,61 +26,60 @@ const procesar = (e) => {
 
     // hago algunas validaciones para procesar el sueldo
     if (s_bruto.value !== "") {
-        r_afp = s_bruto.value * afp;
-        r_ars = s_bruto.value * ars;
+        r_afp = parseFloat(s_bruto.value * afp);
+        r_ars = parseFloat(s_bruto.value * ars);
         t_descuento = r_afp + r_ars;
-        s_neto = s_bruto.value - Math.trunc(t_descuento);
+        s_neto = s_bruto.value - t_descuento;
         r_isr = (s_neto * 12) > exento ? s_neto * 12 : "Exento";
-
         // valido si el monto es excento y procede a hacer el calculo
-        if (r_isr > exento) {
+        if (r_isr >= exento) {
             // forma no. 1 con el 15% del ISR
-            if (r_isr > 399923) {
+            if (r_isr >= exento) {
                 monto = r_isr - exento;
-                impuesto = (monto * isr1) / 12;
+                impuesto = parseFloat((monto * isr1) / 12);
+                t_descuento = r_afp + r_ars + impuesto;
+                s_neto = s_bruto.value - t_descuento;
             }
 
             // forma no. 2 con el 20% del ISR
-            if (r_isr > 599884) {
-                monto = r_isr - exento;
-                impuesto = ((monto * isr2) + 29994) / 12;
+            if (r_isr >= exento2) {
+                monto = r_isr - exento2;
+                impuesto = parseFloat(((monto + 31216) * isr2) / 12);
+                t_descuento = r_afp + r_ars + impuesto;
+                s_neto = s_bruto.value - t_descuento;
             }
-            
+
             // forma no. 3 con el 25% del ISR
-            if (r_isr > 3833171) {
-                monto = r_isr - exento;
-                impuesto = ((monto * isr3) + 76652) / 12;
+            if (r_isr >= exento3) {
+                monto = r_isr - exento3;
+                impuesto = parseFloat(((monto + 79776) * isr3) / 12);
+                t_descuento = r_afp + r_ars + impuesto;
+                s_neto = s_bruto.value - t_descuento;
             }
 
         } else {
             impuesto = r_isr; // si no supera el exento, imprime el mensaje
         }
 
-
         render.innerHTML = `
-        <div class="form-group">
-                <label for="afp">AFP</label>
-                <input type="text" class="form-control" id="afp" value="RD$ ${r_afp}" disabled readonly>
+            <div class="form-group">
+               <p>El descuento de la AFP es: <b>RD$${r_afp.toFixed(2)}</b></p>
             </div> 
             
             <div class="form-group">
-                <label for="ars">ARS</label>
-                <input type="text" class="form-control" id="ars" value="RD$ ${r_ars}" disabled readonly>
+                <p>El descuento del ARS es: <b>RD$${r_ars.toFixed(2)}</b></p>
             </div> 
             
             <div class="form-group">
-                <label for="isr">ISR</label> 
-                <input type="text" class="form-control" id="isr" value="${impuesto}" disabled readonly>
+                <p>El descuento del ISR mensual es de: <b>${isNaN(impuesto) ? impuesto : "RD$" + impuesto.toFixed(2)}</b></p>
             </div> 
             
             <div class="form-group">
-                <label for="descuento">Total descuento</label>
-                <input type="text" class="form-control" id="descuento" value="RD$ ${t_descuento}" disabled readonly>
+                <p>El total de los descuentos es de: <b>RD$${t_descuento.toFixed(2)}</b></p>
             </div> 
             
             <div class="form-group">
-                <label for="sueldo_neto">Sueldo neto</label>
-                <input type="text" class="form-control" id="sueldo_neto" value="RD$ ${s_neto}" disabled readonly>
+                <p>El sueldo a cobrar es de: <b>RD$${s_neto.toFixed(2)}</b></p>
             </div>
         `;
 
@@ -95,7 +97,7 @@ const cancelar = (e) => {
 form.addEventListener("submit", procesar); // evento para el submit
 form.addEventListener("reset", cancelar); // evento para el cancelar de un formulario
 
-/*  
+/*
      ***********   Nota:  ******************
 
     mi logica no es muy buena pero espero que pueda ser entendida
